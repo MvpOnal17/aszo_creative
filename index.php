72f7bb696669622d0af1516c36ee9a0f43322739
MvpOnal17 <mvponal17@gmail.com>
2025-05-01 14:29:27 +0800

    Update:Navbar Jadi Bahasa Indonesia

--- index.php
+++ index.php
@@ -47,12 +47,12 @@ include 'conf/conf.php';
 
             <nav id="navmenu" class="navmenu">
                 <ul>
-                    <li><a href="#home" class="active">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#features">Features</a></li>
-                    <li><a href="#case">Services</a></li>
+                    <li><a href="#home" class="active">Beranda</a></li>
+                    <li><a href="#about">Tentang Kami</a></li>
+                    <li><a href="#features">Fitur</a></li>
+                    <li><a href="#case">Studi Kasus</a></li>
                     <li><a href="#protofolio">Portofolio</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#contact">Kontak Kami</a></li>
                 </ul>
                 <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
             </nav>
